29ccb4890cb9b7b66780eb642f4d57dc1555be13
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-04-07 23:37:18 +0200

    add code for workshop on lazyminting

---
+++ 04-nft-merkle-drop/hardhat.config.js
@@ -0,0 +1,23 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+
+const settings = {
+  optimizer: {
+    enabled: true,
+    runs: 200,
+  },
+};
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      { version: '0.5.16', settings },
+      { version: '0.6.12', settings },
+      { version: '0.7.6',  settings },
+      { version: '0.8.3',  settings },
+    ],
+  },
+};
---
+++ 04-nft-merkle-drop/test/1-ERC721LazyMint.test.js
@@ -0,0 +1,73 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+const tokens = require('./tokens.json');
+
+async function deploy(name, ...params) {
+  const Contract = await ethers.getContractFactory(name);
+  return await Contract.deploy(...params).then(f => f.deployed());
+}
+
+function hashToken(tokenId, account) {
+  return Buffer.from(ethers.utils.solidityKeccak256(['uint256', 'address'], [tokenId, account]).slice(2), 'hex')
+}
+
+
+describe('ERC721MerkleDrop', function () {
+  before(async function() {
+    this.accounts = await ethers.getSigners();
+  });
+
+  describe('Mint all elements', function () {
+    before(async function() {
+      this.registry = await deploy('ERC721LazyMint', 'Name', 'Symbol');
+      await this.registry.grantRole(await this.registry.MINTER_ROLE(), this.accounts[1].address);
+    });
+
+    for (const [tokenId, account] of Object.entries(tokens)) {
+      it('element', async function () {
+        const signature = await this.accounts[1].signMessage(hashToken(tokenId, account));
+        await expect(this.registry.redeem(account, tokenId, signature))
+          .to.emit(this.registry, 'Transfer')
+          .withArgs(ethers.constants.AddressZero, account, tokenId);
+      });
+    }
+  });
+
+  describe('Duplicate mint', function () {
+    before(async function() {
+      this.registry = await deploy('ERC721LazyMint', 'Name', 'Symbol');
+      await this.registry.grantRole(await this.registry.MINTER_ROLE(), this.accounts[1].address);
+
+      this.token = {};
+      [ this.token.tokenId, this.token.account ] = Object.entries(tokens).find(Boolean);
+      this.token.signature = await this.accounts[1].signMessage(hashToken(this.token.tokenId, this.token.account));
+    });
+
+    it('mint once - success', async function () {
+      await expect(this.registry.redeem(this.token.account, this.token.tokenId, this.token.signature))
+        .to.emit(this.registry, 'Transfer')
+        .withArgs(ethers.constants.AddressZero, this.token.account, this.token.tokenId);
+    });
+
+    it('mint twice - failure', async function () {
+      await expect(this.registry.redeem(this.token.account, this.token.tokenId, this.token.signature))
+        .to.be.revertedWith('ERC721: token already minted');
+    });
+  });
+
+  describe('Frontrun', function () {
+    before(async function() {
+      this.registry = await deploy('ERC721LazyMint', 'Name', 'Symbol');
+      await this.registry.grantRole(await this.registry.MINTER_ROLE(), this.accounts[1].address);
+
+      this.token = {};
+      [ this.token.tokenId, this.token.account ] = Object.entries(tokens).find(Boolean);
+      this.token.signature = await this.accounts[1].signMessage(hashToken(this.token.tokenId, this.token.account));
+    });
+
+    it('Change owner - success', async function () {
+      await expect(this.registry.redeem(this.accounts[0].address, this.token.tokenId, this.token.signature))
+        .to.be.revertedWith('Invalid signature');
+    });
+  });
+});
